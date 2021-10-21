750cdc2c80bdc2d7470525161e28a5242a7e7b51
jakewmiles <jake.w.miles@gmail.com>
2021-10-21 11:22:14 +0100

    feat: implement postbook with hardcoded data

--- book/book.controller.go
+++ book/book.controller.go
@@ -9,15 +9,23 @@ func GetBooks(ctx *fiber.Ctx) error {
 	DB.Find(&books)
 	return ctx.JSON(books)
 }
+
 func GetBook(ctx *fiber.Ctx) error {
 	id := ctx.Params("id")
 	var book Book
 	DB.Find(&book, id)
 	return ctx.JSON(book)
 }
+
 func PostBook(ctx *fiber.Ctx) error {
-	return ctx.SendString("New Book!")
+	var book Book
+	book.Title = "1984"
+	book.Author = "George Orwell"
+	book.Rating = 5
+	DB.Create(&book)
+	return ctx.JSON(book)
 }
+
 func DeleteBook(ctx *fiber.Ctx) error {
 	return ctx.SendString("No Book!")
 }
